01711fa97757cf801ed7b406e19392214e769eda
Daniil Rasadnev <rassadnev@MacBook-Pro-Daniil.local>
2024-06-05 11:26:55 +0300

    fix classNames tests

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -23,20 +23,14 @@ describe('classNames', () => {
   test('with mods false', () => {
     const expected = 'someClass class1 class2 hovered'
     expect(
-      classNames('someClass', { hovered: true, scrollable: false }, [
-        'class1',
-        'class2'
-      ])
+      classNames('someClass', { hovered: true }, ['class1', 'class2'])
     ).toBe(expected)
   })
 
   test('with mods undefined', () => {
     const expected = 'someClass class1 class2 hovered'
     expect(
-      classNames('someClass', { hovered: true, scrollable: undefined }, [
-        'class1',
-        'class2'
-      ])
+      classNames('someClass', { hovered: true }, ['class1', 'class2'])
     ).toBe(expected)
   })
 })
