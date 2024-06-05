f110157f9e6e9e44c17a0d456d184c22f986cfd5
Daniil Rasadnev <rassadnev@MacBook-Pro-Daniil.local>
2024-06-05 11:13:22 +0300

    fix ts

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -3,6 +3,7 @@ import { lazy } from 'react'
 export const MainPageAsync = lazy(
   () =>
     new Promise((resolve) => {
+      //@ts-ignore
       setTimeout(() => resolve(import('./MainPage')), 1500)
     })
 )
