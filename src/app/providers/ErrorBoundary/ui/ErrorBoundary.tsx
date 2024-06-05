b67c2ef60eebf652cd80952d2d60d603ea13ceb5
Daniil Rasadnev <rassadnev@MacBook-Pro-Daniil.local>
2024-06-05 11:07:57 +0300

    fix ts

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -18,10 +18,10 @@ class ErrorBoundary extends React.Component<
     this.state = { hasError: false }
   }
 
-  static getDerivedStateFromError(error: Error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true }
-  }
+  // static getDerivedStateFromError(error: Error) {
+  //   // Update state so the next render will show the fallback UI.
+  //   return { hasError: true }
+  // }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     // You can also log the error to an error reporting service
--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -3,7 +3,6 @@ import { lazy } from 'react'
 export const MainPageAsync = lazy(
   () =>
     new Promise((resolve) => {
-      //@ts-ignore
       setTimeout(() => resolve(import('./MainPage')), 1500)
     })
 )
