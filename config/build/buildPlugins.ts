4d8a54068bb0f893e6f6615428fbbb7b69d94854
Daniil Rasadnev <rassadnev@MacBook-Pro-Daniil.local>
2024-06-05 11:02:22 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,26 +1,35 @@
-import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import webpack  from "webpack"
+import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
-export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-            new HtmlWebpackPlugin({
-                template: paths.html,
-            }),
-            new webpack.ProgressPlugin(),
-            new MiniCssExtractPlugin({
-               filename: 'css/[name].[contenthash:8].css',
-               chunkFilename: 'css/[name].[contenthash:8].css',
-            }),
-            new webpack.DefinePlugin({
-                __IS_DEV__: JSON.stringify(isDev),
-            }),
-            new webpack.HotModuleReplacementPlugin(),
-            new BundleAnalyzerPlugin({
-                openAnalyzer: false
-            }),
-        ]
+export function buildPlugins({
+  paths,
+  isDev
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: paths.html
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev)
+    })
+  ]
+
+  if (isDev) {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
+      })
+    )
+  }
+
+  return plugins
 }
